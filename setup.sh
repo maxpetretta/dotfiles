15de058864e0042bdabb004369158d3b1b57db94
Max Petretta <git@maxpetretta.com>
2022-04-19 21:52:14 -0500

    Added README

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+which -s brew
+if [[ $? != 0 ]] ; then
+   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 cp ./bash/.bashrc ~
 cp -a ./bash/functions/. ~/.config/bash/functions
 cp ./ge-proxy/proxy.sh ~/.config/bash/functions
