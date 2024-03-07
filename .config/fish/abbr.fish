5cf088abebaf33174c164e17eb2c758d5026b5bd
Max Petretta <git@maxpetretta.com>
2024-03-07 10:00:09 -0500

    moar cleanup

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -6,9 +6,9 @@ abbr -a ..... cd ../../../..
 abbr -a ...... cd ../../../../..
 
 # ls
-abbr -a ls exa --all --classify
-abbr -a ll exa --all --classify --long --header --git --icons --no-user
-abbr -a lt exa --all --classify --tree --git-ignore --ignore-glob ".git" --level=2
+abbr -a ls eza --all --classify
+abbr -a ll eza --all --classify --long --header --git --icons --no-user
+abbr -a lt eza --all --classify --tree --git-ignore --ignore-glob ".git" --level=2
 
 # git
 abbr -a gi git init
@@ -17,6 +17,8 @@ abbr -a gg git graph
 abbr -a glo git pull origin
 
 # custom
-abbr -a refresh source ~/.config/fish/config.fish
+abbr -a vim nvim
+abbr -a code cursor
 abbr -a prd pnpm run dev 
 abbr -a cf cloudflared tunnel run
+abbr -a refresh source ~/.config/fish/config.fish
